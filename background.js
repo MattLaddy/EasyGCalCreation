f382516305c93c8c0788fd4f6108e12e72ca93f5
MattLaddy <matthewladd19@gmail.com>
2024-07-26 10:18:58 -0400

    addition of Popup css, manifest updates + background functionality added

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "createInvite",
+    title: "Create Calendar Invite",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "createInvite") {
     chrome.identity.getAuthToken({ interactive: true }, function(token) {
       if (chrome.runtime.lastError) {
         console.error(chrome.runtime.lastError);
       } else {
-        console.log("Token acquired: " + token);
-        // Will use this for API request data
+        const eventDetails = encodeURIComponent(info.selectionText);
+        const createEventUrl = `https://calendar.google.com/calendar/r/eventedit?text=${eventDetails}`;
+        
+        chrome.tabs.create({ url: createEventUrl });
       }
     });
-  });
+  }
+});
